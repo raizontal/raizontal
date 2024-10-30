4f6c6ab133d9d189dd4363dca64b129c130fb9f7
esviren <st-rodriguez@hotmail.com>
2024-10-30 12:04:45 -0500

    Add Icon light-bulb

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="p-5 text-center">
         En construcción <UBadge>Nuevo</UBadge>
+        <UIcon name="i-heroicons-light-bulb" class="w-25 h-25" />
     </div>
 </template>
